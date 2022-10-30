cca51fbd2cc5d650a81f0c7ddb9731afa4f2327d
Ricordy <youberrody@gmail.com>
2022-10-30 19:26:58 +0000

    Update: Client_stub almost done, todo: get_height and get_values

--- source/client_stub.c
+++ source/client_stub.c
@@ -63,60 +63,178 @@ int rtree_disconnect(struct rtree_t *rtree){
  * Devolve 0 (ok, em adição/substituição) ou -1 (problemas).
  */
 int rtree_put(struct rtree_t *rtree, struct entry_t *entry){
-struct message_t *msg = malloc(sizeof(struct message_t));
-   if(msg == NULL){
-     return -1;
-   }
-   struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
-   if(msgGRANDE == NULL){
-     return -1;
-   }
-   message_t__init(msgGRANDE);
-   msg->msgConvert = msgGRANDE;
-   msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_PUT;
-   msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_ENTRY;
-   msg->msgConvert->data = entry->value->data;
-   msg->msgConvert->data_size = entry->value->datasize;
-   msg->msgConvert->key = entry->key;
-   msg = network_send_receive(rtree,msg);
-   if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
-     printf("Erro a inserir a entrada!\n");
-     message_t__free_unpacked(msg->msgConvert,NULL);
-     free(msg);
-     return -1;
-   }
-   free(msgGRANDE);
-   message_t__free_unpacked(msg->msgConvert,NULL);
-   free(msg);
+    struct message_t *msg = malloc(sizeof(struct message_t));
+    if(msg == NULL){
+        return -1;
+    }
+    struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
+    if(msgGRANDE == NULL){
+        return -1;
+    }
+    message_t__init(msgGRANDE);
+    msg->msgConvert = msgGRANDE;
+    msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_PUT;
+    msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_ENTRY;
+    msg->msgConvert->data = entry->value->data;
+    msg->msgConvert->data_size = entry->value->datasize;
+    msg->msgConvert->key = entry->key;
+    msg = network_send_receive(rtree,msg);
+    if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
+        printf("Erro a inserir a entrada!\n");
+        message_t__free_unpacked(msg->msgConvert,NULL);
+        free(msg);
+        return -1;
+    }
+    free(msgGRANDE);
+    message_t__free_unpacked(msg->msgConvert,NULL);
+    free(msg);
    
-   //printf("Entrada inserida com sucesso\n");
-   return 0;
+    //printf("Entrada inserida com sucesso\n");
+    return 0;
     
 }
 
 /* Função para obter um elemento da árvore.
  * Em caso de erro, devolve NULL.
  */
-struct data_t *rtree_get(struct rtree_t *rtree, char *key);
+struct data_t *rtree_get(struct rtree_t *rtree, char *key){
+    struct message_t *msg = malloc(sizeof(struct message_t));
+    if(msg == NULL){
+        return NULL;
+    }
+    struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
+    if(msgGRANDE == NULL){
+        return NULL;
+    } 
+    message_t__init(msgGRANDE);
+    msg->msgConvert = msgGRANDE;
+    msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_GET;
+    msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_KEY;
+    msg->msgConvert->key = key;
+    if(msg->msgConvert->key == NULL){
+        free(msg->msgConvert->key);
+        return NULL;
+    }
+    msg = network_send_receive(rtree,msg);
+    if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
+        printf("Erro a obter elemento da tabela\n");
+        message_t__free_unpacked(msg->msgConvert,NULL);
+        free(msg);
+        return NULL;
+    }
+   //printf("Vitoria a obter elemento da tabela\n");
+   struct data_t *dataConvert = data_create2(msg->msgConvert->data_size,msg->msgConvert->data);
+   msg->msgConvert->data = NULL;
+   message_t__free_unpacked(msg->msgConvert,NULL);
+   free(msgGRANDE);
+   free(msg);
+   return dataConvert;
+}
 
 /* Função para remover um elemento da árvore. Vai libertar 
  * toda a memoria alocada na respetiva operação rtree_put().
  * Devolve: 0 (ok), -1 (key not found ou problemas).
  */
-int rtree_del(struct rtree_t *rtree, char *key);
+int rtree_del(struct rtree_t *rtree, char *key){
+    struct message_t *msg = malloc(sizeof(struct message_t));
+    if(msg == NULL){
+        return -1;
+    }
+    struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
+    if(msgGRANDE == NULL){
+        return -1;
+    }
+    message_t__init(msgGRANDE);
+    msg->msgConvert = msgGRANDE;
+    msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_DEL;
+    msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_KEY;
+    msg->msgConvert->key = key;
+    if(msg->msgConvert->key == NULL){
+        return -1;
+    }
+    msg = network_send_receive(rtree,msg);
+    if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
+        printf("Erro a eliminar elemento da tabela\n");
+        message_t__free_unpacked(msg->msgConvert,NULL);
+        free(msg);
+        return -1;
+    }
+    message_t__free_unpacked(msg->msgConvert,NULL);
+    free(msgGRANDE);
+    free(msg);
+    //printf("Vitoria a eliminar elemento da tabela\n");
+    return 0;
+}
 
 /* Devolve o número de elementos contidos na árvore.
  */
-int rtree_size(struct rtree_t *rtree);
+int rtree_size(struct rtree_t *rtree){
+    struct message_t *msg = malloc(sizeof(struct message_t));
+    if(msg == NULL){
+        return -1;
+    }
+    struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
+    if(msgGRANDE == NULL){
+        return -1;
+    }
+    message_t__init(msgGRANDE);
+    msg->msgConvert = msgGRANDE;
+    msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_SIZE;
+    msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+    msg = network_send_receive(rtree,msg);
+    if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
+        printf("Erro a obter numero de elementos da tabela\n");
+        message_t__free_unpacked(msg->msgConvert,NULL);
+        free(msg);
+        return -1;
+    } 
+    int size = msg->msgConvert->sizeofkeys;
+    message_t__free_unpacked(msg->msgConvert,NULL);
+    free(msgGRANDE);
+    free(msg);
+    //printf("Vitoria a obter numero de elementos da tabela\n");
+    return size;
+}
 
 /* Função que devolve a altura da árvore.
  */
-int rtree_height(struct rtree_t *rtree);
+int rtree_height(struct rtree_t *rtree){
+    
+}
 
 /* Devolve um array de char* com a cópia de todas as keys da árvore,
  * colocando um último elemento a NULL.
  */
-char **rtree_get_keys(struct rtree_t *rtree);
+char **rtree_get_keys(struct rtree_t *rtree){
+    struct message_t *msg = malloc(sizeof(struct message_t));
+    if(msg == NULL){
+        return NULL;
+    }
+    struct _MessageT *msgGRANDE = malloc(sizeof(struct _MessageT));
+    if(msgGRANDE == NULL){
+        free(msg);
+        return NULL;
+    }
+    message_t__init(msgGRANDE);
+    msg->msgConvert = msgGRANDE;
+    msg->msgConvert->opcode = MESSAGE_T__OPCODE__OP_GETKEYS;
+    msg->msgConvert->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+
+    msg = network_send_receive(rtree,msg);
+    if(msg->msgConvert->opcode == MESSAGE_T__OPCODE__OP_ERROR){
+        printf("Erro a obter keys da tabela\n");
+        message_t__free_unpacked(msg->msgConvert,NULL);
+        free(msgGRANDE);
+        free(msg);
+        return NULL;
+    }
+    char **keys = msg->msgConvert->keys;
+    msg->msgConvert->keys = NULL;
+    message_t__free_unpacked(msg->msgConvert,NULL);
+    free(msgGRANDE);
+    free(msg);
+    return keys;
+}
 
 /* Devolve um array de void* com a cópia de todas os values da árvore,
  * colocando um último elemento a NULL.
